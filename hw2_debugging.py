af8cb853e32508817648c62f218fee1b021411a7
Krithika Ragothaman <71825347+kr1k-boop@users.noreply.github.com>
2024-09-10 00:31:18 +0000

    Ran pylint and adopted suggested changes for hw2_debugging

---
+++ hw2_debugging.py
@@ -0,0 +1,38 @@
+"""This module consists of two methods that together, perform the Merge Sort algorithm on an array,
+returning a sorted array. """
+import rand
+
+def merge_sort(array):
+    """This function is the parent merge sort algorithm."""
+    if len(array) == 1:
+        return array
+
+    half = len(array)//2
+
+    return recombine(merge_sort(array[:half]), merge_sort(array[half:]))
+
+def recombine(left_array, right_array):
+    """This function sorts and recombines the two arrays."""
+    left_index = 0
+    right_index = 0
+    merged_array = [None] * (len(left_array) + len(right_array))
+    while left_index < len(left_array) and right_index < len(right_array):
+        if left_array[left_index] < right_array[right_index]:
+            right_index += 1
+            merged_array[left_index + right_index] = left_array[left_index]
+        else:
+            left_index += 1
+            merged_array[left_index + right_index] = right_array[right_index]
+
+    for i in range(right_index, len(right_array)):
+        merged_array[left_index + right_index] = right_array[i]
+
+    for i in range(left_index, len(left_array)):
+        merged_array[left_index + right_index] = left_array[i]
+
+    return merged_array
+
+arr = rand.random_array([None] * 20)
+arr_out = merge_sort(arr)
+
+print(arr_out)
