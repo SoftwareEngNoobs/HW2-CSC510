cd6134fcf5af344f75bf009269df0aa31bc6d19d
Mahim Dashora <60029463+mahimdashora@users.noreply.github.com>
2024-09-11 22:23:21 -0400

    Update merge_sort_test.py

--- merge_sort_test.py
+++ merge_sort_test.py
@@ -5,3 +5,6 @@ import hw2_debugging, rand
 def test_merge_random():
     arr = rand.random_array([0] * 20)
     assert hw2_debugging.merge_sort(arr) == sorted(arr)
+def test_duplicates():
+     # Test with an array that has duplicate elements
+    assert merge_sort([4, 2, 2, 8, 3, 3, 1]) == [1, 2, 2, 3, 3, 4, 8], "Sorting an array with duplicates FAILED"
