f2dd8ecb42050145e927641ac00d40d6711e36f9
Krithika Ragothaman <71825347+kr1k-boop@users.noreply.github.com>
2024-09-12 02:53:55 +0000

    Added new test case, updated README w dynamic pylint badge and updated workflow for pylint badge

--- merge_sort_test.py
+++ merge_sort_test.py
@@ -5,3 +5,7 @@ import hw2_debugging, rand
 def test_merge_random():
     arr = rand.random_array([0] * 20)
     assert hw2_debugging.merge_sort(arr) == sorted(arr)
+
+def test_merge_reversed_array():
+    arr = sorted(rand.random_array([0] * 20))[::-1]
+    assert hw2_debugging.merge_sort(arr) == arr[::-1]
